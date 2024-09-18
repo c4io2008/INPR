602d081cd75f61d35e3649805d1fbca53ed18847
c4io2008 <chmb1@aluno.ifal.edu.br>
2024-09-17 23:28:57 -0300

    Atividades kenji

---
+++ 1o-bimestre/lista-2/Q1.js
@@ -0,0 +1,19 @@
+ 
+
+ console.log ("Exemplos de identificadores válidos:");
+
+ console.log ("nomeUsuario");
+ console.log (" idade")
+ console.log (" _contaBancaria");
+ console.log (" precoProduto");
+ console.log (" numero1");
+
+
+ console.log ("Exemplos de identificadores inválidos:");
+
+ console.log (" 2nome( ");
+ console.log ("@idade(@)")
+ console.log ("nome-usuario(");
+ console.log ("preco$($)");
+ console.log ("class(");
+
