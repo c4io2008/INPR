1c49a9bea6e1f06e9b2e08d9f8d545013ea04dc4
Caio Henrique Mariano Bandeira <chmb1@aluno.ifal.edu.br>
2024-09-25 13:35:30 -0300

    Update Q2.js

--- 1o-bimestre/lista-2/Q2.js
+++ 1o-bimestre/lista-2/Q2.js
@@ -1,11 +1,1 @@
-const prompt = require('prompt-sync')({sigint: true});
 
-var nome,idade, idadeFutura;
-
-nome = prompt("Digite o seu nome:");
-idade = prompt("Digite a sua idade:");
-
-idadeFutura = idade + 10;
-
-
-console.log("Olá, " + nome + ". Você terá " + idadeFutura + " anos daqui a 10 anos.");
