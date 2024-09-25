41fa0e2310e98ab9fbad5e8201f7ee2eaaf29761
Caio Henrique Mariano Bandeira <chmb1@aluno.ifal.edu.br>
2024-09-25 13:35:54 -0300

    Update Q4.js

--- 1o-bimestre/lista-2/Q4.js
+++ 1o-bimestre/lista-2/Q4.js
@@ -1,18 +1,11 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-var nota1, nota2, nota3;
-var soma, divisao;
+var anoAtual = 2024;
+var anoNascimento, nome, idade;
 
+nome = prompt("Qual o nome? ");
+anoNascimento = Number(prompt("Qual o ano de nascimento? "));
 
- nota1 = Number (prompt("Digite a nota do primeiro bimestre:"));
- nota2 = Number (prompt("Digite a nota do segundo bimestre:"));
- nota3 = Number (prompt("Digite a nota do terceiro bimestre:"));
- 
- soma = nota1 + nota2 + nota3 ;
- divisao = soma / 3 ;
+idade = anoAtual - anoNascimento;
 
-
- 
-
-
- console.log(` A media do aluno é ${nota1} ${nota2} ${nota3} = ${divisao}`);
+console.log(`Olá, ${nome}, hoje você tem ${idade} anos`);
