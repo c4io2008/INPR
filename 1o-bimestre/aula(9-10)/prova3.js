c26a3c78792992b677b75bb08a9595375128c7f5
c4io2008 <chmb1@aluno.ifal.edu.br>
2024-10-09 15:26:49 -0300

    att

--- 1o-bimestre/aula(9-10)/prova3.js
+++ 1o-bimestre/aula(9-10)/prova3.js
@@ -1,1 +1,13 @@
 // Caio Henrique Mariano Bandeira
+
+const prompt = require("prompt-sync")({sigint:true});
+
+var hora = Number(prompt("<< Quantas horas vc trabalha no mês ?"))
+
+var hora1 = 32 * hora
+
+var imposto = hora1 * 0.27
+var salario = imposto
+
+console.log(`O seu salario é  = ${salario}`);
+
