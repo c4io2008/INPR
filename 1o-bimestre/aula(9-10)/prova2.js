ef1ac92806bcb2fbb03ad786f76d3079bebc0b7c
c4io2008 <chmb1@aluno.ifal.edu.br>
2024-10-09 15:21:04 -0300

    atualizzando

--- 1o-bimestre/aula(9-10)/prova2.js
+++ 1o-bimestre/aula(9-10)/prova2.js
@@ -5,8 +5,12 @@ const prompt = require("prompt-sync")({sigint:true});
 var zinco, cobre;
 var latao = Number(prompt("<< Digite a quantidade de latão em kg:"))
 
-zinco = latao / 70
-cobre = latao / 30
+latao = latao * 1000
+
+
+zinco = `${latao}` * 0.7 
+cobre = `${latao}` * 0.3
+
 
 console.log(`A quantidade de zinco é = ${zinco}`);
 console.log(`A quantidade de cobre é = ${cobre}`);
