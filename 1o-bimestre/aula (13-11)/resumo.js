d48db361e9a2d2fd5c2e4262f4ebd220ed35a269
c4io2008 <chmb1@aluno.ifal.edu.br>
2024-11-27 13:41:07 -0300

    for e while

---
+++ 1o-bimestre/aula (13-11)/resumo.js
@@ -0,0 +1,23 @@
+//const prompt = require("prompt-sync")({sigint:true});
+
+//var nome = prompt("**Digite seu nome:")
+//var ano_nascimento = Number(prompt("**Qual o ano que vc nasceu? "))
+//var ano_atual = Number(prompt("**Qual o ano atual?"))
+
+//var idade = ano_atual - ano_nascimento
+
+//console.log(nome);
+//console.log(idade);
+ 
+for(let i = 8; i >= 0; i--){
+    console.log("falta ", i, "dias para o meu aniversario");
+}
+
+let i = 8
+
+console.log("********************")
+
+while(i >= 0){
+    console.log("falta", i, "dias para o meu aniversario")
+    i--
+}
